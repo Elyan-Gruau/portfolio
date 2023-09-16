88fb0a92a9027c715d969b74f462702ca96bce53
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-16 14:16:26 +0200

    Amélioration des skills

--- V3React/portfolio/src/components/sections/skills/Skills.js
+++ V3React/portfolio/src/components/sections/skills/Skills.js
@@ -1,16 +1,24 @@
 import Skill from "./skill/Skill";
+import "./Skills.css";
 
 function Skills(){
     return (
-        <div className="technology">
+        <div className="skills">
             <h2>Technologies</h2>
-            <p>TODO lors du hover d'une compétence, un bouton tout a droite pop permetant d'afficher les projet métant
-                en oeuvre cette techo</p>
-            <Skill name="Java"/>
-            <Skill name="JavaScript"/>
-            <Skill name="HTML/CSS"/>
-            <Skill name="React"/>
-            <Skill name="Android (Java)"/>
+            {/*TODO lors du hover d'une compétence, un bouton tout a droite pop permetant d'afficher les projet métant*/}
+            {/*en oeuvre cette techo*/}
+            <div className="skillCont">
+                <div className="leftPart">
+                    <Skill name="Java"/>
+                    <Skill name="JavaScript"/>
+                    <Skill name="HTML/CSS"/>
+                    <Skill name="React"/>
+                    <Skill name="Android (Java)"/>
+                </div>
+                <div className="rightPart">
+                    <Skill name="PHP"/>
+                </div>
+            </div>
         </div>
     );
 }
