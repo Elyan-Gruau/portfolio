da960fce550d9c9f815e3b227310decc3248147e
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-19 10:12:36 +0200

    Update Search.js

--- V3React/portfolio/src/components/sections/works/search/Search.js
+++ V3React/portfolio/src/components/sections/works/search/Search.js
@@ -1,8 +1,10 @@
 import "./search.css";
 function Search(){
     return (
+      //   faire en sorte que dans le placeholder, le nom des projets defilent.
       <div className="searchBar">
-            recherche
+          <input id="searchWorkInput" placeholder="Travaux ... " type="text"/>
+          <button id="searchWorkButton"></button>
       </div>
     );
 }
