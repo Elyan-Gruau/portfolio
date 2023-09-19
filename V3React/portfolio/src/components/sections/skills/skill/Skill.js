81653a734e274950f8c150224c342871d3fcf603
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-19 10:12:26 +0200

    Amélioration des skills

--- V3React/portfolio/src/components/sections/skills/skill/Skill.js
+++ V3React/portfolio/src/components/sections/skills/skill/Skill.js
@@ -1,13 +1,32 @@
 import './skill.css'
 function Skill(props){
+    const name = props.name.replace(" ","_").replace("(","").replace(")","").toLowerCase();
+    //const bar = renderBar("--CleanBlack");
+    const barHover = renderBar("--color_"+name);
+    const barStyle = {
+        color: "#fff",
+        backgroundImage: barHover,
+        boxShadow: "none"
+    };
+    const barStyleHover = {
+
+    }
+
+    function renderBar (color) {
+       return "linear-gradient(90deg, var("+color+") "+props.value+"%, transparent "+props.value+"%)"
+    }
+
     return (
         <div className="skill">
             <div className="skillName">
                 <p>{props.name}</p>
             </div>
-            <div className="skillBar">
+            <div className="skillBarValue"  style={barStyle}>
+                <div className="skillBar">
 
+                </div>
             </div>
+
 
             <button className="skillButton">
                 +
