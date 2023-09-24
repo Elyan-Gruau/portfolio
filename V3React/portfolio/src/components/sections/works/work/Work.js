ddbdb32b26dc96732ed0ec31db4c0ada9dcb528d
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-24 20:22:17 +0200

    Adapation au nouveau style

--- V3React/portfolio/src/components/sections/works/work/Work.js
+++ V3React/portfolio/src/components/sections/works/work/Work.js
@@ -1,6 +1,7 @@
 import "./work.css";
 import Technology from "./Technology";
 function Work (props){
+    let imageSrc= "img/projects/"+props.title+".png";
     const technos = [];
     const technoIterrator = props.technolist;
     if (technoIterrator!=null){
@@ -13,18 +14,23 @@ function Work (props){
 
     return(
         <div className="work">
-            <div className="imageContainer"></div>
-            <h2 className="title">{props.title}</h2>
-            <div className="workHeader">
-                <h3 className="date">{props.date}</h3>
-                <p className="person_count">{props.person_count}</p>
-                <p className="state">{props.state}</p>
+            <div className="imageContainer">
+                <img className="workLogo" src={imageSrc} alt={props.title}/>
+            </div>
+            <div className="textContainer">
+                <h2 className="title">{props.title}</h2>
+                <div className="workHeader">
+                    <h3 className="date">{props.date}</h3>
+                    <p className="person_count">{props.person_count}</p>
+                    <p className="state">{props.state}</p>
+                </div>
+
+                <p className="description">{props.description}</p>
+                <div className="workTechnoContainer">
+                    {technos}
+                </div>
             </div>
 
-            <p className="description">{props.description}</p>
-            <div className="workTechnoContainer">
-                {technos}
-            </div>
             {/*<p className="">{props.person_count}</p>*/}
             {/*<p>work, nombre de personnes, description, liste des techno utilisée ... état (terminé, en cours ...)</p>*/}
         </div>
