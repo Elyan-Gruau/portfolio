337c1c8f4160db79201194cbd60d32dc3bd30cc7
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-24 20:17:20 +0200

    Ajout des experiences

--- V3React/portfolio/src/components/sections/experiences/Experiences.js
+++ V3React/portfolio/src/components/sections/experiences/Experiences.js
@@ -1,8 +1,23 @@
+import Experience from "./experience/Experience";
+import "./experiences.css";
 function Experiences () {
     return (
-        <div className="Experiences">
+        <section className="Experiences">
             <h2>Experience</h2>
-        </div>
+            <div className="experiencesContainer">
+                <Experience title="Consultant Technique"
+                            type="Stage"
+                            desc="Conception et développement d'une solution de reporting automatique"
+                            company="Planisware"
+                            companyWebsite="https://fr.planisware.com/"
+                            status="En production"
+                            location="GreenSide Batiment 5, 400 Av. Roumanille Bâtiment 6, 06410 Biot"
+                            date="04 - 08/2022"
+                            technolist={["java","jsp","javascript","sql","git"]}
+                />
+
+            </div>
+        </section>
     );
 }
 export default Experiences;
--- V3React/portfolio/src/components/sections/experiences/experience/Experience.js
+++ V3React/portfolio/src/components/sections/experiences/experience/Experience.js
@@ -1,7 +1,36 @@
+import Technology from "../../works/work/Technology";
+
 function Experience (props) {
+    let imgPath = "/img/companies/"+props.company+".png";
+    const technos = [];
+    const technoIterrator = props.technolist;
+    if (technoIterrator!=null){
+        for (let i=0; i<technoIterrator.length; i++){
+            technos.push(
+                <Technology title={technoIterrator[i]}/>
+            );
+        }
+    }
     return (
         <div className="experience">
+            <a className="companyLogoContainer"
+               href={props.companyWebsite} target="_blank">
+                <img src={imgPath} alt={props.company} />
+            </a>
             <h2 className="title" >{props.title}</h2>
+            <div className="textContainer">
+                <div className="experienceHeader">
+                    <p className="date">{props.date}</p>
+                    <p className="type">{props.type}</p>
+                    <p className="status">{props.status}</p>
+                </div>
+                <p className="description">{props.desc}</p>
+                <p className="location">{props.location}</p>
+                {/*<p>{props.company}</p>*/}
+            </div>
+            <div className="workTechnoContainer">
+                {technos}
+            </div>
         </div>
     );
 }
