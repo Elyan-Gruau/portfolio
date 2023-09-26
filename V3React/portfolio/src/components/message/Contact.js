d400514ec2b699d43be708140e2a3b9d8bce909a
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-26 11:49:43 +0200

    Résolution du bug du blur + adaptation au nouveau style

--- V3React/portfolio/src/components/message/Contact.js
+++ V3React/portfolio/src/components/message/Contact.js
@@ -1,18 +1,20 @@
 import './message.css';
 function Contact(){
     return (
-        <div id="contactForm">
-            <h2>Contact</h2>
-            <form>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" placeholder="test@gmail.com"/>
+        <div id="contactForm" className="movingBackground">
+            <div className="glass">
+                <h2>Contact</h2>
+                <form>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" placeholder="test@gmail.com"/>
 
-                <label htmlFor="message">Message</label>
-                <textarea id="message" placeholder="Rencontrons-nous !"/>
-                <div className="buttonContainer">
-                    <button className="btnSender">Envoyer</button>
-                </div>
-            </form>
+                    <label htmlFor="message">Message</label>
+                    <textarea id="message" placeholder="Rencontrons-nous !"/>
+                    <div className="buttonContainer">
+                        <button className="btnSender">Envoyer</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
