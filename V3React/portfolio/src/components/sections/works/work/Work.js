455702a128e481e4054ff2ac5c87fe94cdbe77ef
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-26 14:12:05 +0200

    Encapsulation en utilisant le glassmorphisme

--- V3React/portfolio/src/components/sections/works/work/Work.js
+++ V3React/portfolio/src/components/sections/works/work/Work.js
@@ -13,26 +13,28 @@ function Work (props){
     }
 
     return(
-        <div className="work">
-            <div className="imageContainer">
-                <img className="workLogo" src={imageSrc} alt={props.title}/>
-            </div>
-            <div className="textContainer">
-                <h2 className="title">{props.title}</h2>
-                <div className="workHeader">
-                    <h3 className="date">{props.date}</h3>
-                    <p className="person_count">{props.person_count}</p>
-                    <p className="state">{props.state}</p>
+        <div className="work movingBackground">
+            <div className="glass">
+                <div className="imageContainer">
+                    <img className="workLogo" src={imageSrc} alt={props.title}/>
+                </div>
+                <div className="textContainer">
+                    <h2 className="title">{props.title}</h2>
+                    <div className="workHeader">
+                        <h3 className="date">{props.date}</h3>
+                        <p className="person_count">{props.person_count}</p>
+                        <p className="state">{props.state}</p>
+                    </div>
+
+                    <p className="description">{props.description}</p>
+                    <div className="workTechnoContainer">
+                        {technos}
+                    </div>
                 </div>
 
-                <p className="description">{props.description}</p>
-                <div className="workTechnoContainer">
-                    {technos}
-                </div>
+                {/*<p className="">{props.person_count}</p>*/}
+                {/*<p>work, nombre de personnes, description, liste des techno utilisée ... état (terminé, en cours ...)</p>*/}
             </div>
-
-            {/*<p className="">{props.person_count}</p>*/}
-            {/*<p>work, nombre de personnes, description, liste des techno utilisée ... état (terminé, en cours ...)</p>*/}
         </div>
     );
 }
