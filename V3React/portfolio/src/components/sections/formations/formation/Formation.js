64c1702315762fd25866ea042e6ea88f801aff9a
gruau <elyan.gruau@etu.univ-cotedazur.fr>
2023-09-28 09:50:09 +0200

    Adaptation du style glassmorphism

--- V3React/portfolio/src/components/sections/formations/formation/Formation.js
+++ V3React/portfolio/src/components/sections/formations/formation/Formation.js
@@ -1,15 +1,17 @@
 import "./formation.css";
 function Formation(props){
     return(
-        <div className="formation">
-            <h2 className="title">{props.title}</h2>
-            <h2 className="subtitle">{props.subtitle}</h2>
-            <h3 className="year">{props.year}</h3>
-            <p className="mention">{props.mention}</p>
-            <p className="description">{props.desc}</p>
-            <p className="location">
-                {props.location}
-            </p>
+        <div className="formation movingBackground">
+            <div className="glass">
+                <h2 className="title">{props.title}</h2>
+                <h2 className="subtitle">{props.subtitle}</h2>
+                <h3 className="year">{props.year}</h3>
+                <p className="mention">{props.mention}</p>
+                <p className="description">{props.desc}</p>
+                <p className="location">
+                    {props.location}
+                </p>
+            </div>
         </div>
     );
 }
