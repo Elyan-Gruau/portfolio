94889c24c98ec20860b7ce76b35577162f6a4301
Elyan-Gruau <elyangruau.pro@gmail.com>
2023-11-18 16:15:03 +0100

    Amélioration de la partie présentation

--- V3React/portfolio/src/components/sections/presentation/Presentation.js
+++ V3React/portfolio/src/components/sections/presentation/Presentation.js
@@ -6,10 +6,8 @@ function Presentation () {
             <h2 id="firstTitle presentationBackground"> Étudiant </h2>
             <h2 id="secondTitle presentationBackground"> Développeur </h2>
             <div id="profile">
-                <div>
-                    <img alt="profilePic"
+                <img alt="profilePic"
                          src="/img/profile-pic.png"/>
-                </div>
             </div>
             ANIMATION D ECRITURE
             MAKE YOUR DREAM POSSIBLE
