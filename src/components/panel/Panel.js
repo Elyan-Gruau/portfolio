0966d923025b80034f5e4a2701d12daf5fdbefaf
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-02-14 08:39:08 +0100

    Ajout du lien de téléchargement du CV

--- src/components/panel/Panel.js
+++ src/components/panel/Panel.js
@@ -7,7 +7,7 @@ function Panel (props){
                 <a href="#work">Travaux</a>
                 <a href="#experience">Experience</a>
                 <a href="#contactForm">Contact</a>
-                <a href="https://github.com/Elyan-Gruau/portfolio">Curiculum</a>
+                <a href="/docs/GRUAU%20Elyan%20CV.pdf" download>Curiculum</a>
             </div>
         </div>
     );
