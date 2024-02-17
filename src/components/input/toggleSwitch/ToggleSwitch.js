e83ed54a88a48674692c8c7d7105039471d110de
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-02-17 22:46:09 +0100

    Fix de la propagation des evenements

--- src/components/input/toggleSwitch/ToggleSwitch.js
+++ src/components/input/toggleSwitch/ToggleSwitch.js
@@ -1,11 +1,14 @@
 import './toggleSwitch.css';
 export default function ToggleSwitch (props) {
     return (
-        <div className="toggleSwitch" onClick={props.onClick} >
+        <div className="toggleSwitch" >
             <label className="switch">
                 <input type="checkbox" id={"toggleSwitch_"+props.id}
                        disabled={props.disabled}
-                       onClick={(e) => e.stopPropagation()}
+                       onClick={(e) => {
+                           props.onClick();
+                           e.stopPropagation()
+                       }}
                 />
                 <span className="slider"></span>
             </label>
