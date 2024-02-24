dfd4136b4287a3413195c25c5229907be22ece4f
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-02-24 12:19:27 +0100

    Amélioration du footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,10 +4,27 @@ function Footer(){
         <footer id="contact" >
             <div className="glass">
                 <div className="lookLikeButton">
-                    <a href="https://www.paypal.com/cgi-bin/webscr?">Mail</a>
-                    <a href="https://www.linkedin.com/in/elyan-gruau-a62397243/"  target="_blank" rel="noreferrer">Linkedin</a>
-                    <a href="https://github.com/Elyan-Gruau"  target="_blank" rel="noreferrer">Github</a>
-                    <a href=" https://discordapp.com/users/484843311849537536" target="_blank" rel="noreferrer">Discord</a>
+                    <a href="mailto:elyangruau.pro@gmail.com?subject=Entretient"
+                       target="_blank"
+                       rel="noreferrer"
+                       className="button_mail">
+                    </a>
+                    <a href="https://www.linkedin.com/in/elyan-gruau-a62397243/"
+                       target="_blank"
+                       rel="noreferrer"
+                       className="button_linkedin">
+                    </a>
+                    <a href="https://github.com/Elyan-Gruau"
+                       target="_blank"
+                       rel="noreferrer"
+                       className="button_github">
+                    </a>
+                    {/*<a href=" https://discordapp.com/users/484843311849537536"*/}
+                    {/*   target="_blank"*/}
+                    {/*   rel="noreferrer"*/}
+                    {/*   className="button_discord">*/}
+                    {/*    Discord*/}
+                    {/*</a>*/}
                 </div>
                 <span className="author">
                 Développé par Elyan GRUAU - Tous droits réservés © - 2023
