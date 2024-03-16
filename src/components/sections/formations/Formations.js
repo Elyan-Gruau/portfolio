3cd02dc959ff090a58cf077a2faee5c32a5038d6
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-03-16 18:51:44 +0100

    Amélioration des formations

--- src/components/sections/formations/Formations.js
+++ src/components/sections/formations/Formations.js
@@ -10,13 +10,14 @@ function Formations(){
                 <div className="lifeLine">
                 </div>
                 <Formation title="Baccalauréat général"
+                           subtitle="Équivalent BAC S"
                            year="2021"
-                           mention="Mention : bien"
+                           mention="Mention bien"
                            desc="Spécialités : Numerique et science de l'informatique (NSI), Mathématiques, Sciences de l'ingénieur"
                            location="Lycée Thomas Edison, 83510 Lorgues" />
 
                 <Formation title="DUT Informatique"
-                           subtitle="Diplome Universitaire de Technologie"
+                           subtitle="Diplome Universitaire de Technologie Informatique"
                            year="2021 - 2023"
                            desc="Parcours Réalisation d’applications : conception, développement, validation"
                            location="IUT Nice Côte d'Azur, 06000 Nice" />
@@ -25,7 +26,7 @@ function Formations(){
                 <Formation title="L3 MIASHS - MIAGE"
                            subtitle="Mathématiques et informatique appliquées aux sciences humaines et sociales"
                            year="2023 - 2024"
-                           mention="Mention: Major de promotion"
+                           mention="Major de promotion"
                            desc="Parcours Méthodes Informatiques Appliquées à la Gestion des Entreprises"
                            location="1645 Rte des Lucioles, 06410 Biot" />
 
--- src/components/sections/formations/formation/Formation.js
+++ src/components/sections/formations/formation/Formation.js
@@ -1,12 +1,19 @@
 import "./formation.css";
 function Formation(props){
+
+    const Mention = () => {
+        if (props.mention){
+            return <p className="mention">{props.mention}</p>
+        }
+    }
+
     return(
         <div className="formation movingBackground ">
             <div className="glass">
                 <h2 className="title">{props.title}</h2>
                 <h2 className="subtitle">{props.subtitle}</h2>
                 <h3 className="year">{props.year}</h3>
-                <p className="mention">{props.mention}</p>
+                <Mention/>
                 <p className="description">{props.desc}</p>
                 <p className="location">
                     {props.location}
