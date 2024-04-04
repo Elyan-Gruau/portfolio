968e074f0be40a6a37d1d811e306ff567dee50dd
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-04-04 14:58:49 +0200

    Amélioration des skills

--- src/components/sections/skills/skill/Skill.js
+++ src/components/sections/skills/skill/Skill.js
@@ -5,8 +5,7 @@ function Skill(props){
     // const name= "allSkill";
     const barStyle = {
         color: "#fff",
-        width: props.value+"%",
-        boxShadow: "none"
+        width: props.value+"%"
     };
     // const barStyleHover = {
     //
@@ -19,9 +18,9 @@ function Skill(props){
             <div className="skillName">
                 <p>{props.name}</p>
             </div>
-            <div className="skillBarContainer movingBackground tooltip" >
+            <div className="skillBarContainer tooltip" >
                 <span className="tooltiptext">{props.value}/100</span>
-                <div className="skillBarGlass glass">
+                <div className="skillBarGlass">
                     <div className="skillBarValue "  style={barStyle}>
 
                     </div>
