569ac18c93b41323991b5f98be8f7ce7dcddf74b
Elyan-Gruau <elyangruau.pro@gmail.com>
2025-03-06 14:50:34 +0100

    Add app path

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss', '.css'],
     alias: {
+      App: path.resolve(__dirname, 'src/App.js'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@style': path.resolve(__dirname, 'src/style'),
       '@assets': path.resolve(__dirname, 'src/assets'),
